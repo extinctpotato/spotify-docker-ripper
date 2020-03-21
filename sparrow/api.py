41b20aa465606f8742693c9e19a25e43c14cb1a9
Adam Olech <nddr89@gmail.com>
2020-03-21 18:51:53 +0100

    sparrow/api.py: added status endpoint

--- sparrow/api.py
+++ sparrow/api.py
@@ -5,7 +5,8 @@ pickle.HIGHEST_PROTOCOL = 4
 from threading import Lock
 from rq import Queue
 from rq.job import Job
-from sparrow import is_track_uri, spotify_start, spotify_stop
+from sparrow import is_track_uri, spotify_start, spotify_stop, dbus_env, is_spotify_running
+from sparrow import SpotifyInterface
 
 sparrow_api = Flask(__name__)
 redis_con = Redis()
@@ -18,16 +19,35 @@ T = 9600
 def index():
     return make_response("Argh!", 200)
 
+@sparrow_api.route("/status", methods=["GET"])
+def test_route():
+    dbus_env()
+    s = SpotifyInterface()
+    j = {}
+    j.update(s.get_meta())
+    j['playback_status'] = s.get_property('PlaybackStatus')
+    j['pending_jobs'] = len(q.jobs)
+    j['spotify_running'] = is_spotify_running()
+    return make_response(jsonify(j), 200)
+
 @sparrow_api.route("/spotify/<string:action>", methods=["POST"])
 def s(action):
     if action == "start":
-        spotify_start()
+        if not is_spotify_running():
+            q.enqueue_call(func='sparrow.spotify_start', timeout=5)
+            msg = "OK"
+        else:
+            msg = "RUNNING"
     elif action == "stop":
-        spotify_stop()
+        if is_spotify_running():
+            q.enqueue_call(func='sparrow.spotify_stop', timeout=5)
+            msg = "OK"
+        else:
+            msg = "STOPPED"
     else:
         j = jsonify({"msg":"No such action!"})
         return make_response(j, 404)
-    j = jsonify({"msg":"OK"})
+    j = jsonify({"msg":msg})
     return make_response(j, 200)
     
 
