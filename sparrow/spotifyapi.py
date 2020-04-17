2b6f941f600d87b9f3aec57d2cd1802a1479a055
Adam Olech <nddr89@gmail.com>
2020-04-17 21:04:12 +0200

    sparrow/spotifyapi.py: added episode endpoint

--- sparrow/spotifyapi.py
+++ sparrow/spotifyapi.py
@@ -55,3 +55,30 @@ class Track:
     def download_cover(self):
         r = requests.get(self.cover_url)
         self.cover_bytes = r.content
+
+class Episode:
+    def __init__(self, eid="spotify:episode:5lUORBC9mw9vroABUJj5A9", market="US"):
+        self.eid = strip_tid(eid)
+        self.market = market
+        self.token = get_token()
+        self.headers = {"Authorization":"Bearer {}".format(self.token)}
+        self.metadata = self.__get_meta()
+        self.cover_url = self.__get_cover()[0]
+        self.cover_bytes = None 
+        self.cover_dimensions = self.__get_cover()[1:]
+
+    def __get_meta(self):
+        url = "https://api.spotify.com/v1/episodes/{}".format(self.eid)
+        r = requests.get(url, headers=self.headers, params={'market':self.market})
+        j = r.json()
+        del j['show']['available_markets']
+        return j
+
+    def __get_cover(self):
+        for cover in self.metadata['images']:
+            if cover['height'] > 600 and cover['width'] > 600:
+                return cover['url'], cover['height'], cover['width']
+
+    def download_cover(self):
+        r = requests.get(self.cover_url)
+        self.cover_bytes = r.content
